96ed1c20d5a03ffb8ef2fff7c5483538279b7f44
Barry Allen <babanov-artem@yandex.ru>
2025-09-30 18:04:17 +0600

    add time increment function to CommonDateTime

--- src/common/utils/CommonDateTime.hpp
+++ src/common/utils/CommonDateTime.hpp
@@ -1,5 +1,4 @@
-#ifndef COMMON_UTILS_COMMONDATETIME_HPP
-#define COMMON_UTILS_COMMONDATETIME_HPP
+#pragma once
 
 #include "stdint.h"
 #include <string>
@@ -35,6 +34,40 @@ public:
         return yearFrom2000 == 0 && month == 1 && day == 1; //for now, when we still can't travel in time, it's seems enough
     }
 
+    void incrementSeconds(uint8_t s){
+        uint16_t t = seconds + s;
+        if (t > 59){
+            incrementMinutes(t / 60);
+            seconds = t % 60;
+        }else{
+            seconds = t;
+        }
+    }
+    
+    void incrementMinutes(uint8_t m){
+        uint16_t t = minutes + m;
+        if (t > 59){
+            incrementHours(t / 60);
+            minutes = t % 60;
+        }else{
+            minutes = t;
+        }
+    }
+
+    void incrementHours(uint8_t h){
+        uint16_t t = hours + h;
+        if (t > 23){
+            incrementDays(t / 24);
+            hours = t % 24;
+        }else{
+            hours = t;
+        }
+    }
+
+    void incrementDays(uint8_t d){
+        day += d;
+    }
+
 public:
     uint8_t yearFrom2000 = 0;
     uint8_t month = 1;
@@ -43,5 +76,3 @@ public:
     uint8_t minutes = 0;
     uint8_t seconds = 0;
 };
-
-#endif
